e762dfa78b190f9a388fae8eccf5a46f193fd8fe
AmalBabuAR <amalbabuar@gmail.com>
2023-08-09 20:59:04 +0530

    add server the webhook for stripe and router,add new secrdd

--- server/routes/stripeWebhook.js
+++ server/routes/stripeWebhook.js
@@ -11,15 +11,17 @@ const stripeWebhookRouter = express.Router();
 
 // This is your Stripe CLI webhook secret for testing your endpoint locally.
 const endpointSecret = "whsec_cdfb269e3f39af946052f256d392a9df8016bf6ab19e82f978aa8953259462ee";
+const endpointSecretss = "whsec_yCX9xg27T9WQKkHEqUoIfvhdBOhwmCFf";
 
 stripeWebhookRouter.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
   const sig = request.headers['stripe-signature'];
   console.log('payload', request);
+  console.log('sign', sig);
 
   let event;
 
   try {
-    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecretss);
   } catch (err) {
     console.log(err);
     response.status(400).send(`Webhook Error: ${err.message}`);
