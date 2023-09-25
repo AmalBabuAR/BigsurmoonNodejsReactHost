eb1b4b4a98f62ad6cc4f34e57cdf78574288fd1a
AmalBabuAR <amalbabuar@gmail.com>
2023-09-25 21:07:58 +0530

    change the google Analytic code

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import Router from "./Routers/Routes.jsx";
-import GoogleAnalytics from "./Components/GoogleAnalytics/GoogleAnalytics.jsx";
+// import GoogleAnalytics from "./Components/GoogleAnalytics/GoogleAnalytics.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
-    <GoogleAnalytics />
+    {/* <GoogleAnalytics /> */}
     <RouterProvider router={Router} />
   </>
 );
