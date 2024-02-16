81ba60714e24754b6436f1a6567b62a1b49dc42f
AmalBabuAR <amalbabuar@gmail.com>
2024-02-16 14:29:11 +0530

    added meta pixel

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import Router from "./Routers/Routes.jsx";
+import MetaPixel from "./utils/Meta/MetaPixel.jsx";
 // import GoogleAnalytics from "./Components/GoogleAnalytics/GoogleAnalytics.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
     {/* <GoogleAnalytics /> */}
+    <MetaPixel />
     <RouterProvider router={Router} />
   </>
 );
