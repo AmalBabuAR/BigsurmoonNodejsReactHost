b2adcffab0f2128aad7802aa8d6600b285945c95
AmalBabuAR <amalbabuar@gmail.com>
2024-03-05 12:03:28 +0530

    updates in video tag frontend

--- frontend/src/Components/V2Home/HomeScreenOne.jsx
+++ frontend/src/Components/V2Home/HomeScreenOne.jsx
@@ -6,15 +6,15 @@ import { useNavigate } from "react-router-dom";
 const HomeScreenOne = () => {
   const navigate = useNavigate();
   return (
-    <div className="w-full bg-black text-white font-roboto h-auto lg:h-auto xl:h-auto mx-auto overflow-hidden">
+    <div className="w-full h-auto mx-auto overflow-hidden text-white bg-black font-roboto lg:h-auto xl:h-auto">
       <div className="min-w-[320px] max-w-[450px] lg:max-w-[1280px] xl:max-w-[1920px] mx-auto mt-0 relative">
         <video
-          autoPlay
-          muted
-          preload="auto"
           playsInline
+          muted
+          autoPlay
           loop
-          className=" xl:hidden lg:hidden w-screen max-w-[450px] mx-auto h-screen"
+          preload="auto"
+          className="mobVideo xl:hidden lg:hidden w-screen max-w-[450px] mx-auto h-screen"
         >
           <source src={homeVideoMob} type="video/mp4" />
         </video>
@@ -23,7 +23,7 @@ const HomeScreenOne = () => {
           muted
           loop
           playsInline
-          className="videot hidden xl:block lg:block"
+          className="hidden videot xl:block lg:block"
           id="videot"
           height="100%"
           preload="auto"
@@ -38,7 +38,7 @@ const HomeScreenOne = () => {
         <div className="absolute top-[35vh] xl:top-0 lg:top-0 left-0 right-0 mx-auto">
           <div className="flex flex-col justify-center items-center homeCreateVirtual py-[25px] lg:py-0 xl:py-0 text-center gap-[15px] lg:gap-[25px] xl:gap-[25px] lg:mt-[90px]">
             <h1 className="text-[26px] lg:text-[100px] xl:text-[100px] leading-[26px] lg:leading-[100px] xl:leading-[100px] font-bold blogTextShadow">
-              Create Virtual <br className="lg:block xl:block hidden" />
+              Create Virtual <br className="hidden lg:block xl:block" />
               Experiences
             </h1>
             <p className="text-[16px] lg:text-[24px] xl:text-[24px] leading-[20px] lg:leading-[36px] xl:leading-[36px] flex flex-col homeHead">
