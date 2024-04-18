72b8b890b97b8289a0590610f6fc63ae30302783
AmalBabuAR <amalbabuar@gmail.com>
2024-04-19 02:57:00 +0530

    v2 editor fifth update

--- server/public/v2Viewer/functionScript.js
+++ server/public/v2Viewer/functionScript.js
@@ -53,8 +53,11 @@ async function setupModelViewer() {
 		setupEnvironment(
 			modelViewer,
 			data?.environment,
-			data?.config?.useEnvAsSkybox
+			data?.config?.useEnvAsSkybox,
+			data?.config?.skyboxHeight
 		);
+
+		setUpBestPractices(modelViewer, data?.bestPractices);
 	} catch (error) {
 		console.log(error);
 	}
@@ -97,7 +100,7 @@ function setUpConfig(modelViewer, config) {
 	if (autoplay) {
 		setUpAutoPlay(modelViewer);
 	}
-	
+
 	if (autoRotate) {
 		modelViewer.autoRotate = autoRotate;
 	}
@@ -269,14 +272,32 @@ function setUpAutoPlay(modelViewer) {
 	});
 }
 
-function setupEnvironment(modelViewer, env, skybox) {
+function setupEnvironment(modelViewer, env, skybox, skyboxHeight) {
 	if (env) {
 		const envImage = new Image();
 		envImage.src = `../../editor/hdrImages/${env}`;
 		modelViewer.environmentImage = envImage.src;
 		if (skybox) {
 			modelViewer.skyboxImage = envImage.src;
+			if (skyboxHeight) {
+				modelViewer.skyboxHeight = skyboxHeight;
+			}
 		}
+	}
+}
+
+function setUpBestPractices(modelViewer, bestPractices) {
+	if (bestPractices?.arButton) {
+		createArButton(modelViewer);
+		modelViewer.ar = true;
+	}
+
+	if (bestPractices?.progressBar) {
+		createProgressBar(modelViewer);
+	}
+
+	if (bestPractices?.arPrompt) {
+		createArPrompt(modelViewer);
 	}
 }
 
@@ -350,3 +371,63 @@ mobConfig.addEventListener("click", function () {
 	document.getElementById("controlsContainerCollapse").style.display = "flex";
 	mobConfig.style.display = "none";
 });
+
+function createArButton(modelViewer) {
+	// Create button element
+	const button = document.createElement("button");
+	button.setAttribute("slot", "ar-button");
+	button.setAttribute("id", "ar-button");
+
+	// Create SVG element
+	const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+	svg.setAttribute("width", "20");
+	svg.setAttribute("height", "21");
+	svg.setAttribute("viewBox", "0 0 25 26");
+	svg.setAttribute("fill", "none");
+
+	// Create path element within SVG
+	const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+	path.setAttribute(
+		"d",
+		"M23.7393 8.75134C24.5588 8.75134 25 8.30463 25 7.49806V4.48274C25 1.82728 23.588 0.4375 20.8522 0.4375H17.7887C16.9692 0.4375 16.5154 0.871805 16.5154 1.67837C16.5154 2.47253 16.9692 2.90683 17.7887 2.90683H20.6884C21.8356 2.90683 22.4912 3.50245 22.4912 4.7061V7.49806C22.4912 8.30463 22.9324 8.75134 23.7393 8.75134ZM1.26072 8.75134C2.08018 8.75134 2.50883 8.30463 2.50883 7.49806V4.7061C2.50883 3.50245 3.15179 2.90683 4.31165 2.90683H7.2113C8.03076 2.90683 8.48462 2.47253 8.48462 1.67837C8.48462 0.871805 8.03076 0.4375 7.2113 0.4375H4.14776C1.42461 0.4375 0 1.81487 0 4.48274V7.49806C0 8.30463 0.453858 8.75134 1.26072 8.75134ZM12.5063 12.263C14.4478 12.2754 16.0237 10.6375 16.0237 8.47835C16.0237 6.45573 14.4478 4.78055 12.5063 4.78055C10.5522 4.78055 8.98891 6.45573 8.98891 8.47835C8.98891 10.6375 10.5522 12.2506 12.5063 12.263ZM6.39183 19.5593H18.6208C19.2259 19.5593 19.5285 19.1498 19.5285 18.6039C19.5285 17.1272 17.3096 13.3302 12.5063 13.3302C7.70298 13.3302 5.48412 17.1272 5.48412 18.6039C5.48412 19.1498 5.78669 19.5593 6.39183 19.5593ZM4.14776 25.044H7.2113C8.03076 25.044 8.48462 24.5973 8.48462 23.8031C8.48462 23.009 8.03076 22.5622 7.2113 22.5622H4.31165C3.15179 22.5622 2.50883 21.979 2.50883 20.7754V17.9834C2.50883 17.1769 2.06757 16.7301 1.26072 16.7301C0.441251 16.7301 0 17.1769 0 17.9834V20.9987C0 23.6542 1.42461 25.044 4.14776 25.044ZM17.7887 25.044H20.8522C23.588 25.044 25 23.6542 25 20.9987V17.9834C25 17.1769 24.5461 16.7301 23.7393 16.7301C22.9198 16.7301 22.4912 17.1769 22.4912 17.9834V20.7754C22.4912 21.979 21.8356 22.5622 20.6884 22.5622H17.7887C16.9692 22.5622 16.5154 23.009 16.5154 23.8031C16.5154 24.5973 16.9692 25.044 17.7887 25.044Z"
+	);
+	path.setAttribute("fill", "white");
+
+	// Append path to SVG
+	svg.appendChild(path);
+
+	// Create span element
+	const span = document.createElement("span");
+	span.textContent = "View in your room";
+
+	// Append SVG and span to button
+	button.appendChild(svg);
+	button.appendChild(span);
+
+	modelViewer.appendChild(button);
+}
+
+function createProgressBar(modelViewer) {
+	const progressBarContainer = document.createElement("div");
+	progressBarContainer.setAttribute("class", "progress-bar hide");
+	progressBarContainer.setAttribute("slot", "progress-bar");
+	const updateBar = document.createElement("div");
+	updateBar.setAttribute("class", "update-bar");
+	progressBarContainer.appendChild(updateBar);
+	modelViewer.appendChild(progressBarContainer);
+}
+
+function createArPrompt(modelViewer) {
+	// Create the div element
+	const arPromptDiv = document.createElement("div");
+	arPromptDiv.setAttribute("id", "ar-prompt");
+
+	// Create the image element
+	const image = document.createElement("img");
+	image.setAttribute("src", "../../v2Viewer/ar_hand_prompt.png");
+
+	// Append the image to the div
+	arPromptDiv.appendChild(image);
+
+	modelViewer.appendChild(arPromptDiv);
+}
